f7b825acbd48b4ef77dfa1aa1a7f8574f63197f7
Atul Soam <atul.soam@hcltech.com>
2025-01-13 22:08:43 +0530

    something

--- blinkit/services/config.tsx
+++ blinkit/services/config.tsx
@@ -2,12 +2,12 @@
 // export const BASE_URL = "http://192.168.31.223:3000/api";
 // export const SOCKETURL = "http://192.168.31.223:3000";
 // For server
-// export const BASE_URL = "https://balajifoods-7qaj.onrender.com/api";
-// export const SOCKETURL = "https://balajifoods-7qaj.onrender.com";
+export const BASE_URL = "https://balajifoods-7qaj.onrender.com/api";
+export const SOCKETURL = "https://balajifoods-7qaj.onrender.com";
 
 // For gher Wifi
-export const BASE_URL = "http://192.168.138.140:3000/api";
-export const SOCKETURL = "http://192.168.138.140:3000";
+// export const BASE_URL = "http://192.168.138.140:3000/api";
+// export const SOCKETURL = "http://192.168.138.140:3000";
 
 export const GOOGleMapsApiKey = "AIzaSyDErj9crYvl3jeZ2sXQ5rVMSkSyklc6eh0";
 
